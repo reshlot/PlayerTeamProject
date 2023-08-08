3507108d0e157e5bce80694a2b435e2eeec35625
HP <reshmaksriet@gmail.com>
2023-08-08 10:03:22 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerTeam
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Team team = new Team();
+
+                while (true)
+                {
+                Console.WriteLine("***FasatPace Cricket Academy***\n");
+                Console.WriteLine("1. Add player");
+                    Console.WriteLine("2. Remove player");
+                    Console.WriteLine("3. Get player details by Id");
+                    Console.WriteLine("4. Get player details by name");
+                    Console.WriteLine("5. Get all player details");
+                    Console.WriteLine("6. Exit");
+                    Console.WriteLine("Enter your choice: ");
+
+                    int choice = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter player Id: ");
+                            int playerId = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter player name: ");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Enter player age: ");
+                            int age = int.Parse(Console.ReadLine());
+                            team.AddPlayer(playerId, name, age);
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter player Id to remove: ");
+                            int playerIdToRemove = int.Parse(Console.ReadLine());
+                            team.RemovePlayer(playerIdToRemove);
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter player Id to get details: ");
+                            int playerIdToGet = int.Parse(Console.ReadLine());
+                            team.GetPlayerDetailsById(playerIdToGet);
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter player name to get details: ");
+                            string nameToGet = Console.ReadLine();
+                            team.GetPlayerDetailsByName(nameToGet);
+                            break;
+                        case 5:
+                            team.GetAllPlayerDetails();
+                            break;
+                        case 6:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+            }
+        
+    }
+
+}
+
---
+++ Team.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerTeam
+{
+    public  class Team
+    {
+        private List<Player> players;
+
+        public Team()
+        {
+            players = new List<Player>();
+        }
+
+        public void AddPlayer(int playerId, string name, int age)
+        {
+            if (players.Count < 11)
+            {
+                Player player = new Player
+                {
+                    PlayerId = playerId,
+                    Name = name,
+                    Age = age
+                };
+                players.Add(player);
+                Console.WriteLine(" ***Player added successfully.***");
+            }
+            else
+            {
+                Console.WriteLine("Cannot add more than 11 players to the team.!!!");
+            }
+        }
+
+        public void RemovePlayer(int playerId)
+        {
+            Player player = players.Find(p => p.PlayerId == playerId);
+            if (player != null)
+            {
+                players.Remove(player);
+                Console.WriteLine("***Player removed successfully.***");
+            }
+            else
+            {
+                Console.WriteLine("Player not found.");
+            }
+        }
+
+        public void GetPlayerDetailsById(int playerId)
+        {
+            Player player = players.Find(p => p.PlayerId == playerId);
+            if (player != null)
+            {
+                Console.WriteLine($"Player Id: {player.PlayerId}");
+                Console.WriteLine($"Name: {player.Name}");
+                Console.WriteLine($"Age: {player.Age}");
+            }
+            else
+            {
+                Console.WriteLine("Player not found.!!!!");
+            }
+        }
+
+        public void GetPlayerDetailsByName(string name)
+        {
+            List<Player> matchingPlayers = players.FindAll(p => p.Name == name);
+            if (matchingPlayers.Count > 0)
+            {
+                foreach (Player player in matchingPlayers)
+                {
+                    Console.WriteLine($"Player Id: {player.PlayerId}");
+                    Console.WriteLine($"Name: {player.Name}");
+                    Console.WriteLine($"Age: {player.Age}");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Player not found.!!!");
+            }
+        }
+
+        public void GetAllPlayerDetails()
+        {
+            if (players.Count > 0)
+            {
+                foreach (Player player in players)
+                {
+                    Console.WriteLine($"Player Id: {player.PlayerId}");
+                    Console.WriteLine($"Name: {player.Name}");
+                    Console.WriteLine($"Age: {player.Age}");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("No players in the team.!!!");
+            }
+        }
+    }
+}
+
